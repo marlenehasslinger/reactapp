4205eb1067a64a369e4126038e91b6bff43644b8
Marlene Hasslinger <mh254@hdm-stuttgart.de>
2019-03-29 20:21:45 -0400

    Remove Raidum

--- src/App.js
+++ src/App.js
@@ -50,10 +50,6 @@ class App extends Component {
       border: '1px solid blue',
       padding: '8px',
       cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'lightgreen',
-        color: 'black'
-      }
     };
 
     let persons = null;
