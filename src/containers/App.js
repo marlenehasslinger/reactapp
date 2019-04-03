cf4264ec840a62dcd4fa60b981a39007088514f0
Marlene Hasslinger <mh254@hdm-stuttgart.de>
2019-04-03 10:14:58 -0400

    Add Lifecycle Update Logs

--- src/containers/App.js
+++ src/containers/App.js
@@ -28,6 +28,16 @@ class App extends Component {
     console.log('[App.js] componentDidMount')
   }
 
+  shouldComponentUpdate() {
+    console.log('App.js - shouldComponentUpdate');
+    return true;
+
+  }
+
+  componentDidUpdate() {
+    console.log('App.js - componentDidUpdate');
+  }
+
   nameChangedHandler = ( event, id ) => {
     const personIndex = this.state.persons.findIndex( p => {
       return p.id === id;
